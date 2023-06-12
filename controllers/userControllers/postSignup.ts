29c3b2c719b0042dc08f1a2e78860c27ef1bd92f
JugurtaO <jugurtaourzik@gmail.com>
2023-06-13 00:54:23 +0200

    +master: simple changes

--- controllers/userControllers/postSignup.ts
+++ controllers/userControllers/postSignup.ts
@@ -4,13 +4,16 @@ import * as myModels from "../../models/index";
 
 export  const postSignup=async (req: Request,res: Response)=>{
 
+    //get user credentials from request's body
     const {user_nickname,user_email,user_password}:{user_nickname:string,user_email:String,user_password:string } = req.body;
+
+
     if(!user_nickname.length ||!user_email.length ||!user_password.length)
         return res.send("credentials can not be blank!")
 
-    const newUser = myModels.User.create({user_nickname:user_nickname,user_email:user_email,user_password:user_password});
+    const newUser = await myModels.User.create({user_nickname:user_nickname,user_email:user_email,user_password:user_password});
 
-
+    
     
     
     return res.send("OK.");
